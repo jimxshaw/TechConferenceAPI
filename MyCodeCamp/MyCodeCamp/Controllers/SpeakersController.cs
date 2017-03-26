a573d688f33a5cf1679bad5aa5f09f47c26c6b41
Jim Shaw <jimxshaw@gmail.com>
2017-03-25 19:33:25 -0500

    Initial implementation of SpeakersController.

---
+++ MyCodeCamp/MyCodeCamp/Controllers/SpeakersController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyCodeCamp.Data;
+
+namespace MyCodeCamp.Controllers
+{
+    [Route("api/camps/{moniker}/speakers")]
+    public class SpeakersController : BaseController
+    {
+        private IMapper _mapper;
+        private ILogger<SpeakersController> _logger;
+        private ICampRepository _repository;
+
+        public SpeakersController(ICampRepository repository,
+                                    ILogger<SpeakersController> logger,
+                                    IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string moniker)
+        {
+            var speakers = _repository.GetSpeakersByMoniker(moniker);
+
+            return Ok(speakers);
+        }
+    }
+}
