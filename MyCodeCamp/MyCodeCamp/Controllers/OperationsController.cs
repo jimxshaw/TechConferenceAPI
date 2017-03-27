1212b44872a46c687677dd1842dc0b99378c4cf6
Jim Shaw <jimxshaw@gmail.com>
2017-03-26 19:19:55 -0500

    Created a functional api with OperationsController.

---
+++ MyCodeCamp/MyCodeCamp/Controllers/OperationsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace MyCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class OperationsController : Controller
+    {
+        private ILogger<OperationsController> _logger;
+        private IConfigurationRoot _config;
+
+        public OperationsController(ILogger<OperationsController> logger, IConfigurationRoot config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        // We're using the Options verb because this action is not any of the usual Get, Post, Put or
+        // Delete verb.
+        [HttpOptions("reloadConfig")]
+        public IActionResult ReloadConfiguration()
+        {
+            try
+            {
+                // IConfigurationRoot has the support to reload configurations whatever the resource is,
+                // such as a json file or an environment variable. 
+                _config.Reload();
+
+                return Ok("Configuration reloaded");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception thrown while reloading configuration: {ex}");
+            }
+
+            return BadRequest("Could not reload configuration");
+        }
+    }
+}
