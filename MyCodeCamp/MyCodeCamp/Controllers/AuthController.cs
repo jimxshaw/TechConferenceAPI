c0747d3bd5fc99fb9bb22f5b8a3788a0da6e5ffd
Jim Shaw <jimxshaw@gmail.com>
2017-05-30 11:23:51 -0500

    Updated CreateToken action in AuthController.

--- MyCodeCamp/MyCodeCamp/Controllers/AuthController.cs
+++ MyCodeCamp/MyCodeCamp/Controllers/AuthController.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using MyCodeCamp.Data;
 using MyCodeCamp.Data.Entities;
 using MyCodeCamp.Filters;
 using MyCodeCamp.Models;
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MyCodeCamp.Controllers
@@ -15,13 +19,19 @@ namespace MyCodeCamp.Controllers
         private CampContext _context;
         private SignInManager<CampUser> _signInManager;
         private ILogger<AuthController> _logger;
+        private UserManager<CampUser> _userMgr;
+        private IPasswordHasher<CampUser> _hasher;
 
         public AuthController(CampContext context,
                                 SignInManager<CampUser> signInManager,
+                                UserManager<CampUser> userMgr,
+                                IPasswordHasher<CampUser> hasher,
                                 ILogger<AuthController> logger)
         {
             _context = context;
             _signInManager = signInManager;
+            _userMgr = userMgr;
+            _hasher = hasher;
             _logger = logger;
         }
 
@@ -48,11 +58,36 @@ namespace MyCodeCamp.Controllers
 
         [ValidateModel]
         [HttpPost("api/auth/token")]
-        public IActionResult CreateToken([FromBody] CredentialModel model)
+        public async Task<IActionResult> CreateToken([FromBody] CredentialModel model)
         {
             try
             {
+                var user = await _userMgr.FindByNameAsync(model.UserName);
+                if (user != null)
+                {
+                    if (_hasher.VerifyHashedPassword(user, user.PasswordHash, model.Password) == PasswordVerificationResult.Success)
+                    {
+                        var claims = new[]
+                        {
+                            new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                        };
 
+                        // In an actual production app, never put the key directly in the code. Place it
+                        // in config settings for example.
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("UNIQUELONGKEYVALUETHATISSECURE"));
+
+                        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                        var token = new JwtSecurityToken(
+                                issuer: "http://mycodecamp.org",
+                                audience: "http://mycodecamp.org",
+                                claims: claims,
+                                expires: DateTime.UtcNow.AddMinutes(15),
+                                signingCredentials: creds
+                        );
+                    }
+                }
             }
             catch (Exception ex)
             {
